fd765b91ae53073375fefb9ef7048c739e63f9c2
tobi <onatadetobi212@gmail.com>
2025-11-08 10:25:38 +0100

    Jwt token prod

--- config/settings.py
+++ config/settings.py
@@ -101,8 +101,10 @@ REST_FRAMEWORK = {
 
 # JWT Settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
 }
 
 
