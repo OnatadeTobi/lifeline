d7e2ca3bd5578b17c6e9e4f5cf9993b3f2cbe7a5
tobi <onatadetobi212@gmail.com>
2025-11-08 11:15:15 +0100

    fix allowed hosts for deployment

--- config/settings.py
+++ config/settings.py
@@ -32,9 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-#ALLOWED_HOSTS = []
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # CSRF_TRUSTED_ORIGINS = [
 #     'https://86270035b70d.ngrok-free.app', 'http://127.0.0.1:8000'
